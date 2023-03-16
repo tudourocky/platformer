1102f73cc9972e4187f90d814e6d2fbbb4a69d20
Evan <111394939+EvanCin@users.noreply.github.com>
2023-03-16 12:23:02 -0700

    Movement update
    Finished implementing WASD and left shift for sprint (Added trail affect)

--- Platformer/Assets/PlayerMovement.cs
+++ Platformer/Assets/PlayerMovement.cs
@@ -3,17 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+
+    //WASD
     private float horizontal;
     private float speed = 8f;
     private float jumpPower = 16f;
     private bool isFacingRight = true;
 
+    //Dash
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashPower = 24f;
+    private float dashTime = 0.2f;
+    private float dashCoolDown = 0.5f;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private TrailRenderer tr;
 
     // Update is called once per frame
     void Update() {
+        if(isDashing) {
+            return;
+        }
         horizontal = Input.GetAxisRaw("Horizontal");
         if(Input.GetButtonDown("Jump") && IsGrounded()) {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
@@ -22,8 +34,14 @@ public class PlayerMovement : MonoBehaviour {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
         Flip();
+        if(Input.GetKeyDown(KeyCode.LeftShift) && canDash) {
+            StartCoroutine(Dash());
+        }
     }
     private void FixedUpdate() {
+        if(isDashing) {
+            return;
+        }
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
     private bool IsGrounded() {
@@ -36,5 +54,19 @@ public class PlayerMovement : MonoBehaviour {
             localScale.x *= -1f;
             transform.localScale = localScale;
         }
+    }
+    private IEnumerator Dash() {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashCoolDown);
+        canDash = true;
     }
 }
