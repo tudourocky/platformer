1a78c5e3ff3ee22905b8bc7638b1da386c88fb2a
tudourocky <67175442+tudourocky@users.noreply.github.com>
2023-06-07 22:11:43 -0700

    Added start and option menu

---
+++ Platformer/Assets/VolumeSaveController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSaveController : MonoBehaviour
+{
+    [SerializeField] private Slider volumeSlider = null;
+
+    private void Start()
+    {
+        LoadValues();
+    }
+
+    public void SaveVolumeButton()
+    {
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue", volumeValue);
+        LoadValues();
+    }
+
+    void LoadValues()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
+    }
+}
