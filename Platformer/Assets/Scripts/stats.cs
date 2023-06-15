a0e9f56774c607389520298b8aa502e67deb6f96
Evan <111394939+EvanCin@users.noreply.github.com>
2023-06-15 10:10:32 -0700

    Healthbars finished

--- Platformer/Assets/Scripts/stats.cs
+++ Platformer/Assets/Scripts/stats.cs
@@ -7,11 +7,13 @@ public class stats : MonoBehaviour
     public Animator animator;
     public int maxHealth = 100;
     int currentHealth;
+    public HealthBar healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
 
     }
 
@@ -27,6 +29,7 @@ public class stats : MonoBehaviour
         Debug.Log("P1 takes damage");
         animator.SetTrigger("hurt");
         currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
         if (currentHealth <= 0)
         {
             die();
--- Platformer/Assets/Scripts/stats2.cs
+++ Platformer/Assets/Scripts/stats2.cs
@@ -7,10 +7,12 @@ public class stats2 : MonoBehaviour
     public Animator animator;
     public int maxHealth = 100;
     int currentHealth;
+    public HealthBar2 healthBar;
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
@@ -24,6 +26,7 @@ public class stats2 : MonoBehaviour
         Debug.Log("P2 takes damage");
         animator.SetTrigger("hurt");
         currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
         if(currentHealth <= 0)
         {
             die();
